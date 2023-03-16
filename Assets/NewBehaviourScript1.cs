aba64972ed213d355a70cf4fa7bebcab88e54862
aliagus1 <magus@edu.cdv.pl>
2023-03-16 15:56:06 +0100

    hahahahaha funny comment

--- Assets/NewBehaviourScript1.cs
+++ Assets/NewBehaviourScript1.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NewBehaviourScript1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
-
-        // another funny comment
     }
 
     // Update is called once per frame
