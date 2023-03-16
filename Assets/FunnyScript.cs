631ffd80b214e0e2b2486cb3a33555c7bb7eabca
Adam Pabianiak <apabianiak@edu.cdv.pl>
2023-03-16 15:52:12 +0100

    HAHAHAAFunnyCommit

--- Assets/FunnyScript.cs
+++ Assets/FunnyScript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FunnyScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //AnotherFunnyComment
+        
     }
 
     // Update is called once per frame
