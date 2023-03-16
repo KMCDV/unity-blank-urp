718eb7825f252d64c358123fcfa50263a88a014b
RostyslavRudenko <rrudenko@CDV-N30223>
2023-03-16 15:53:12 +0100

    added some not funny scripts

--- Assets/notfunnyscript.cs
+++ Assets/notfunnyscript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class notfunnyscript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //i wrote here a joke
+        //its not funny script
     }
 
     // Update is called once per frame
