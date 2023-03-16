c92fa1f934f0e4f1809a2e05437c478434ac2a43
VOLKAN BAYRAK <vbayrak@edu.cdv.pl>
2023-03-16 15:10:41 +0100

    my first commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
